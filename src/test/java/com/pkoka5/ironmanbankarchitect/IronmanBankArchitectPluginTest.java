77c6b111eaad7b1649349cf6d83c9ccfdc08cf71
PKoka5 <181833474+PKoka5@users.noreply.github.com>
2026-07-07 22:12:44 +0200

    Add local RuneLite development launcher

--- src/test/java/com/pkoka5/ironmanbankarchitect/IronmanBankArchitectPluginTest.java
+++ src/test/java/com/pkoka5/ironmanbankarchitect/IronmanBankArchitectPluginTest.java
@@ -2,10 +2,18 @@ package com.pkoka5.ironmanbankarchitect;
 
 import static org.junit.Assert.assertEquals;
 
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
 import org.junit.Test;
 
 public class IronmanBankArchitectPluginTest
 {
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(IronmanBankArchitectPlugin.class);
+		RuneLite.main(args);
+	}
+
 	@Test
 	public void pluginNameIsDefined()
 	{
